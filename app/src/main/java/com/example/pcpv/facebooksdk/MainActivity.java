14b7fc2119ef4dae2a0f0bf7d30235aa87a8362a
thonguyen <dinhthohcmus@gmail.com>
2017-10-16 19:42:49 +0700

    add features to get photo and name

--- app/src/main/java/com/example/pcpv/facebooksdk/MainActivity.java
+++ app/src/main/java/com/example/pcpv/facebooksdk/MainActivity.java
@@ -8,21 +8,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+
+    private ProfilePictureView profilePictureView;
+    private ProfileTracker profileTracker;
+    private AccessTokenTracker accessTokenTracker;
+
+    TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +42,71 @@ public class MainActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
 
+        userName = (TextView)findViewById(R.id.pfName);
+        profilePictureView = (ProfilePictureView)findViewById(R.id.image);
+
+
+        //login with Facebook login Button
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onCancel() {
-                Toast.makeText(MainActivity.this, "Đã đăng xuất", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Login attempt canceled.", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onError(FacebookException e) {
-                Toast.makeText(MainActivity.this, "Đăng nhập thất bại", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Login attempt failed.", Toast.LENGTH_LONG).show();
             }
         });
 
+        // check login or not
+        accessTokenTracker= new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                displayMessage(newProfile);
+            }
+        };
+
+        // check login or not
+        accessTokenTracker.startTracking();
+        // get Photo and Name in profile
+        profileTracker.startTracking();
+        if (isLoggedIn()){
+            userName.setVisibility(View.VISIBLE);
+            profilePictureView.setVisibility(View.VISIBLE);
+        }
+
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    //Hàm kiểm tra trạng thái đăng nhập
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    private void displayMessage(Profile profile) {
+        if (profile != null) {
+            userName.setText(profile.getName());
+            profilePictureView.setProfileId(profile.getId());
+
+        }
     }
 }
